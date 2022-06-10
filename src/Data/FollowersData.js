f7f4bd6614132dc9d50efda7b3ddf310950ddf21
tikaramac19 <tikaramac19@gmail.com>
2022-06-10 18:28:53 +0545

    follwers objects

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,30 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+
+const Followers = [
+        {
+            name : "Anjal Thapa",
+            username :"Taerakkk",
+            img : img1
+        },
+        {
+            name : "Bibas Giri",
+            username :"bibasgiri",
+            img : img2
+        },
+        {
+            name : "Prashant stha",
+            username :"johnwick",
+            img : img3
+        },
+        {
+            name : "Manish Himalaye",
+            username :"mwizard",
+            img : img4
+        }
+];
+
+export default Followers;
