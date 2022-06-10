b0f94cabdc2bc038c9acc501bf912e664b542405
tikaramac19 <tikaramac19@gmail.com>
2022-06-10 18:54:20 +0545

    followers added

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./ProfileSide.css"
 import { LogoSearch } from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
+import FollowersCard from '../FollowersCard/FollowersCard'
 const ProfileSide = () => {
   return (
    <>
@@ -9,6 +10,7 @@ const ProfileSide = () => {
    <div className="ProfileSide">
     <LogoSearch />
     <ProfileCard />
+    <FollowersCard />
    </div>
    
    </>
