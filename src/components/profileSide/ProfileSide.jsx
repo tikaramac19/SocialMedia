e83ec6ef684f45c7ed99afc47c04849674f7ddf9
tikaramac19 <tikaramac19@gmail.com>
2022-06-10 12:10:55 +0545

    small modifiacation

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./ProfileSide.css"
 import { LogoSearch } from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
 const ProfileSide = () => {
   return (
    <>
    
    <div className="ProfileSide">
     <LogoSearch />
+    <ProfileCard />
    </div>
    
    </>
