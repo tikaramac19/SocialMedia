b4dc6a4e1910e165e50fd131f5d5e64ef519af27
tikaramac19 <tikaramac19@gmail.com>
2022-06-11 08:00:13 +0545

    postshare created

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostShare = () => {
+  return (
+    <div>Hello there</div>
+  )
+}
+
+export default PostShare
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./Home.css"
 import ProfileSide from '../../components/profileSide/ProfileSide'
+import PostShare from '../../components/PostShare/PostShare'
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide />
-        <div className="postSide">
-            Posts
-        </div>
+        <PostShare />
         <div className="RightSide">
             RightSide
         </div>
