a5df4cc760ea98f4d26ad0421f49fca969c58e3d
tikaramac19 <tikaramac19@gmail.com>
2022-06-11 19:11:29 +0545

    postShare complited

---
+++ src/components/PostSide/PostSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostShare from '../PostShare/PostShare';
+import "./PostSide.css";
+const PostSide = () => {
+  return (
+    <div className="PostSide">
+       <PostShare />
+       {/* post component */}
+    </div>
+  )
+}
+
+export default PostSide
