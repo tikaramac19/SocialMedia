b6166e5b9191e9321ffd4dd1e19a04969d2048ef
tikaramac19 <tikaramac19@gmail.com>
2022-06-12 11:37:35 +0545

    right side component completed

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./RightSide.css"
+import Home from "../../img/home.png"
+import Notification from "../../img/noti.png"
+import Comment from "../../img/comment.png"
+import {UilSetting} from '@iconscout/react-unicons'
+import TrendCard from '../TrendCard/TrendCard'
+
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+       <div className="navIcons">
+           <img src={Home} alt="" />
+           <UilSetting />
+           <img src={Notification} alt="" />
+           <img src={Comment} alt="" />
+       </div>
+       
+        <TrendCard/> 
+
+        <button className='button rbtn'>
+            Share
+        </button>
+    
+
+    </div>
+  )
+}
+
+export default RightSide
