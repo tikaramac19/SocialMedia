205e5bba216ebab6b152373c64ee75a88a58474e
tikaramac19 <tikaramac19@gmail.com>
2022-06-12 09:56:07 +0545

    created postData file

---
+++ src/Data/PostData.js
@@ -0,0 +1,28 @@
+import postPic1 from "../img/postpic1.jpg"
+import postPic2 from "../img/postpic2.jpg"
+import postPic3 from "../img/postpic3.JPG"
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name :"Tikaram Ac",
+        desc: "Happy New Year all friends ! #2023",
+        likes : 1200,
+        liked : true
+    },
+    {
+        img: postPic2,
+        name :"Parasar Baral",
+        desc: "Tasbir aafai bolxa",
+        likes : 1340,
+        liked : false
+    },
+    {
+        img : postPic3,
+        name :"Sandip Sky",
+        desc: "Have a nice day",
+        likes : 1560,
+        liked : false
+    }
+]
