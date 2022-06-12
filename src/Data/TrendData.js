3707404930a525b7eab245ddee79c1778e26fd78
tikaramac19 <tikaramac19@gmail.com>
2022-06-12 11:36:50 +0545

    created trendData

---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+        name : "Avater 2",
+        shares : 64.21
+    },
+    {
+        name : "Strenger Things",
+        shares : 44.53
+    },
+    {
+        name : "Kabaddi 4",
+        shares : 40.44
+    },
+    {
+        name : "Avengers",
+        shares : 39.53
+    },
+    {
+        name : "KGF",
+        shares : 37.45
+    }
+]
