b5745c37e16cf4cc5f37f679513d906d4007d1f3
tikaramac19 <tikaramac19@gmail.com>
2022-06-12 11:55:18 +0545

    profile com initialize

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Profile.css"
+const Profile = () => {
+  return (
+    <div className="Profile">
+        This is profile Component
+    </div>
+  )
+}
+
+export default Profile
