41ea237ba7ee9046485eb986d23b272f4518eac4
tikaramac19 <tikaramac19@gmail.com>
2022-06-14 16:24:43 +0545

    profile info section complited

--- src/components/ProfileAddress/ProfileAddress.jsx
+++ src/components/ProfileAddress/ProfileAddress.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import "./ProfileAddress.css"
+import {UilPen} from "@iconscout/react-unicons"
 const ProfileAddress = () => {
   return (
       <>
       
       <div className="ProfileAddress">
-            <div className="address">
-                <h3>Address</h3>
+            <div className="info-section">
+              <h1>Your Info</h1>
+              <UilPen style ={{cursor: "pointer"}}/>
+            </div>
+            <div className="more-info">
+              <div><span>Status</span> in Relationship</div>
+              <div><span>Lives</span> in Surkhet</div>
+              <div><span>Works at</span> Broadways Infosys</div>
+
             </div>
 
+            <div className="logout">
+              <button className='button'>Log Out</button>
+            </div>
       </div>
       
       </>
