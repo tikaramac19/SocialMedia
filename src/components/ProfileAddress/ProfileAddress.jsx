1300d16ba274a5e71eaba7cd4a08e8340d363ac9
tikaramac19 <tikaramac19@gmail.com>
2022-06-14 10:38:23 +0545

    working with profile address components

---
+++ src/components/ProfileAddress/ProfileAddress.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./ProfileAddress.css"
+const ProfileAddress = () => {
+  return (
+      <>
+      
+      <div className="ProfileAddress">
+            <div className="address">
+                <h3>Address</h3>
+            </div>
+
+      </div>
+      
+      </>
+  )
+}
+
+export default ProfileAddress
