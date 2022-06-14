b9a6f4afe81a8690c3593265473fb08fa5430be4
tikaramac19 <tikaramac19@gmail.com>
2022-06-14 16:27:12 +0545

    profileLeft and profileRight complited

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from "./Profile.module.css"
-
+import ProfileMiddle from "../../components/ProfileMiddle/ProfileMiddle"
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import ProfileRight from '../../components/ProfileRight/ProfileRight'
+import RightSide from "../../components/RightSide/RightSide"
 const Profile = () => {
   return (
     <>
     <div className={styles.Profile}>
       <ProfileRight />
+      <ProfileMiddle />
+      <RightSide />
     </div>
     
     </>
