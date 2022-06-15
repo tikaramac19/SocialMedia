e8b2737cd78c019ec947a44abc9cc5fa3707d19e
tikaramac19 <tikaramac19@gmail.com>
2022-06-15 20:53:39 +0545

    making follw btn clickable

--- src/components/ProfileRight/ProfileRight.jsx
+++ src/components/ProfileRight/ProfileRight.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import "./ProfileRight.css"
 import { LogoSearch } from '../LogoSearch/LogoSearch'
 import FollowersCard from '../FollowersCard/FollowersCard'
 import ProfileAddress from '../ProfileAddress/ProfileAddress'
 const ProfileRight = () => {
+  const [follow, setFollow] = useState({name:"",follow:false})
+
+const handleFollow = (name)=>{
+setFollow()
+}
+
   return (
     <div className="ProfileRight">
         <LogoSearch />
             {/* profile adddress here */}
         <ProfileAddress />
 
-        <FollowersCard />
+        <FollowersCard handleFollow={handleFollow} follow = {follow}/>
     </div>
   )
 }
